918cadb87b3616f2096514826cb355febc522492
Yuliia2391 <shmyrovajulia@gmail.com>
2023-07-02 15:14:48 +0200

    js_hw_08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listItems = document.querySelector('.gallery');
+
+function createMarkupGalleryItems() {
+    return galleryItems.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}" 
+            alt="${description}"
+            />
+        </a>
+    </li>
+    `)
+    .join('');
+}
+
+listItems.insertAdjacentHTML('beforeend', createMarkupGalleryItems());
+
+let gallery = new SimpleLightbox('.gallery__item a',
+    {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
 
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInputs, 500));
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.target.reset();
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+
+    console.log(formData);
+};
+
+function onFormInputs(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+};
+
+function populateInputs() {
+    const savedInputs = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+    if (savedInputs) {
+        formData = JSON.parse(savedInputs);
+
+        for (const key in formData) {
+            // console.log(form.elements);
+            form.elements[key].value = formData[key];
+        }
+    }
+};
+
+populateInputs();
